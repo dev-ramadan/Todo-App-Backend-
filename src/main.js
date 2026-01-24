81bba4115274986d800b2a99f64ed5f9e5cb4e2b
dev-ramadan <ganna123adam@gmail.com>
2026-01-24 20:16:06 +0200

    install cors

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import connection from "./db/connection.js"
 import authRoute from "./routes/auth.route.js";
 import todoRoute from "./routes/todo.route.js";
 import userRoute from "./routes/user.route.js";
+import cors from "cors"
 const root = async (app) => {
     app.use(express.json())
+    app,use(cors())
     await connection();
 
     app.use(authRoute);
