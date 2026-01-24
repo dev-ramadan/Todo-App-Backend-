52aa3b67fa131b5a5c790a189d73b0536830b341
dev-ramadan <ganna123adam@gmail.com>
2026-01-24 20:22:45 +0200

    test

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connection = async () => {
     try{
-        await mongoose.connect(process.env.DB_URL);
+        await mongoose.connect(process.env.URI);
         console.log('connection success');
         
     }
