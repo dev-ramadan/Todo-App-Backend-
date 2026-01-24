6c9cf9c03221496d2dc3a733f434c6158160b9b8
dev-ramadan <ganna123adam@gmail.com>
2026-01-24 21:06:20 +0200

    deploy

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connection = async () => {
     try{
-        await mongoose.connect(process.env.URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('connection success');
         
     }
